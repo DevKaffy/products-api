5f59d36efa8be2cd3f33be11c474ab372e033c53
DevKaffy <kafilatadewumi61@gmail.com>
2023-08-10 21:00:09 +0100

    responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Home from './Pages/Home/Home';
 import ProductDetail from './Pages/ProductDetail/ProductDetail';
 import NotFound from './Pages/NotFound/NotFound';
-import All from './Components/All/All';
+
 
 const App = () => {
   const router = createBrowserRouter([
