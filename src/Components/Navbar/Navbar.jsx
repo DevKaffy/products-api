8c872d302253824997ca1fee14571a1fdaea3709
DevKaffy <kafilatadewumi61@gmail.com>
2023-08-24 11:09:25 +0100

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="nav-bar">
       <Link to='/'>
       <div className="flex gap-4 items-center mb-8 lg:mb-0">
-        <div className="bg-[white] h-10 w-10 rounded-[10px] flex align-center justify-center shadow-md">
+        <div className="bg-[white] h-12 w-12 rounded-[10px] flex align-center justify-center border-2">
           <img src="/acme.svg" alt="" className="w-4 h-4 self-center" />
         </div>
         <h2 className="font-bold">ACME STORE</h2>
@@ -20,7 +20,7 @@ const Navbar = () => {
         <div>Stickers</div>
       </div>
       <StatusBar />
-      <div className="border-[#737373] border-1 h-10 w-10 rounded-[5px] flex align-center justify-center lg:m-4 mb-8 shadow-md">
+      <div className="border-2 h-12 w-12 rounded-[5px] flex align-center justify-center lg:m-4 mb-8">
         <img src="/cart.svg" alt="" className="w-5 h-5 self-center" />
       </div>
     </div>
