66f2107aa0ffc29653a781d3640de0ced7956a01
DevKaffy <kafilatadewumi61@gmail.com>
2023-08-25 10:40:49 +0100

    fixes:nav-bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className="nav-bar">
+      <div class='flex gap-12'>
       <Link to='/'>
       <div className="flex gap-4 items-center mb-8 lg:mb-0">
         <div className="bg-[white] h-12 w-12 rounded-[10px] flex align-center justify-center border-2">
@@ -19,9 +20,12 @@ const Navbar = () => {
         <div>Shirts</div>
         <div>Stickers</div>
       </div>
+      </div>
+      <div class='flex items-center gap-x-[25rem]'>
       <StatusBar />
       <div className="border-2 h-12 w-12 rounded-[5px] flex align-center justify-center lg:m-4 mb-8">
         <img src="/cart.svg" alt="" className="w-5 h-5 self-center" />
+      </div>
       </div>
     </div>
   );
